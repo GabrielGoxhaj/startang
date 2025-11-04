2bbbb3a934aa892bc9b477eaa5e241e39bdd5221
Gabriel Goxhaj <gabrielgoxhaj2013@hotmail.it>
2025-11-04 12:09:18 +0100

    Add property, event, and two-way binding samples - Second Part Morning 04/11/2025
    Enhanced the app with examples of property binding, event binding, and two-way binding in Angular. Added new UI elements and supporting component properties for demonstration, including style/class bindings, input fields, checkboxes, and select dropdowns.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,31 +1,41 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
+
 export class App {
   protected readonly title = signal('Ciao ragazzi, aperitivo???');
   ciclilavarizia = 'https://www.ciclilavariazia.it/';
   Fullname: string = 'La Varizia Cicli';
+  selectedOption: string = 'red';
   myStyle = {
     color: 'blue',
     fontSize: '20px',
     fontWeight: 'bold',
   };
   btnDisabled: boolean = false;
+  hColor = 'red';
+  hFontSize = '24px';
+  isMyButtonActive: boolean = true;
+  isMyDivActive: boolean = true;
+  isChecked: boolean = false;
+
   GetFullName() {
     return this.Fullname;
   }
   btnCliccami(firstName: string) {
     this.Fullname = `Benvenuto alla Varizia Cicli, ${firstName}!`;
-    return this.Fullname;
+    this.btnDisabled = !this.btnDisabled;
   }
-  onSubmit(event: Event){
-    
+  onSubmit(event: Event) {
+    event.preventDefault();
   }
   InputChange(event: Event) {
     const value = (event.target as HTMLInputElement).value;
