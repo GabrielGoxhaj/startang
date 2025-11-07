59e1b94af78ed7894a13fb3a34fb2ba00ed97996
Gabriel Goxhaj <gabrielgoxhaj2013@hotmail.it>
2025-11-07 10:26:25 +0100

    Updated index.html and main.ts - 5th November 2025 [Not Completed!]

---
+++ src/app/feature/parent-component/parent-component.ts
@@ -0,0 +1,12 @@
+import { Component, signal } from '@angular/core';
+
+@Component({
+  selector: 'parent-component',
+  standalone: true,
+  templateUrl: './parent-component.html',
+  styleUrls: ['./parent-component.css'],
+})
+
+export class ParentComponent {
+    
+}
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { ParentComponent } from './app/feature/parent-component/parent-component'
 
-bootstrapApplication(App, appConfig)
+bootstrapApplication(ParentComponent, appConfig)
   .catch((err) => console.error(err));
