6eac78dbe5ad28ffaed0dfd7c883345e73b6e409
Gabriel <gabrielgoxhaj2013@hotmail.it>
2025-11-10 09:32:06 +0100

    fix: fixed customer.ts import

--- src/app/feature/customer/customer.ts
+++ src/app/feature/customer/customer.ts
@@ -1,57 +1,40 @@
-<!-- <p>customer works!</p> -->
-<button (click)="GetCompleteCustomers()">Ottieni Clienti</button>
-<!-- <br>
-<p>{{ customersData | json }}</p> -->
-<br>
+import { Component } from '@angular/core';
+import { Httprequest } from '../../shared/services/httprequest';
+import { JsonPipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
+import { CustomerModel } from '../../shared/services/models/customer';
+import { RouterLink } from "@angular/router";
 
-@if(customersData.length == 0)
-{<p>Richiesta CLienti NON eseguita</p>}
-@else {<p>ELenco Nominativo CLienti</p>}
-
-<!-- <p *ngIf="!customerOK">Richiesta CLienti NON eseguita</p> -->
-<!-- <ul>
-  <li *ngFor="let cust of customersData;first as i;last as j;index as k;odd as o;even as e">
-    {{ cust.firstName }} - {{ cust.lastName }} (FIrstROw: {{ i }} LastRow {{ j }} INDICE {{ k }} Dispari {{ o }} Pari
-    {{ e }})
-  </li>
-</ul> -->
-
-<ul>
-@for (cust of customersList; track cust.customerId)
-{
-  <li [routerLink]="['/customerdetail', cust.customerId]">
-    {{ cust.firstName }} - {{ cust.lastName }}
-  </li>
-} @empty {<p>Non ci sono CLienti da visualizzare</p>}
-</ul>
-<!-- <div [ngSwitch]="marvelCharacters"> Personaggio Marvel preferito:
-  <p *ngSwitchCase="'ironman'">Iron</p>
-  <p *ngSwitchCase="'spiderman'">Spidy</p>
-  <p style="color: green;" *ngSwitchCase="'The Hulk'">Il Gigante Verde</p>
-</div> -->
-
-<!-- @switch (marvelCharacters) 
-{
-  @case ('ironman') 
-    {<p>Iron</p>} 
-  @case ('spiderman') 
-    {<p>Spidy</p>} 
-  @case ('The Hulk') 
-    {<p style="color: green;">Il Gigante Verde</p>} 
-  @default 
-    {<p>Personaggio non trovato</p>}
-} -->
-
-
-<!-- @for (cust of customersList; track cust.customerId)
-{
-  @switch (cust.title) {
-    @case ('Mr.') 
-      {<li>Signor {{ cust.firstName }} - {{ cust.lastName }}</li>} 
-    @case ('Ms.') 
-      {<li>Signora {{ cust.firstName }} - {{ cust.lastName }}</li>} 
-    @default 
-      {<li>{{ cust.firstName }} - {{ cust.lastName }}</li>}
+@Component({
+  selector: 'app-customer',
+  imports: [CommonModule, RouterLink],
+  templateUrl: './customer.html',
+  styleUrl: './customer.css',
+})
+export class Customer {
+  constructor(private http: Httprequest) {
   }
-  
-} @empty {<p>Non ci sono CLienti da visualizzare</p>} -->
+
+  customersData: any[] = [];
+  marvelCharacters = "The Hulk";
+  customersList: CustomerModel[] = [];
+
+  GetCustomers() {
+    this.http.GetCust().subscribe(data => {
+      this.customersData = data;
+    });
+  }
+
+  GetCompleteCustomers() {
+    this.http.GetCust().subscribe({
+      next: (response) => {
+        console.log('Fetched customers:', response);
+        // this.customersData = response.data;
+        this.customersList = response.data as CustomerModel[];
+      },
+      error: (err) => {
+        console.error('Error fetching customers:', err);
+      }
+    });
+  }
+}
